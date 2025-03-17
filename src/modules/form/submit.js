3c83807bb845e480b6b3b14c0b44abc838df2b88
Andre Maurell <amaurell@gmail.com>
2025-03-17 17:35:10 -0300

    Pegou os dados para fazer o agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 //Data atual para o input
@@ -12,5 +13,33 @@ selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
-  console.log("enviado");
+
+  try {
+    //Recuperando o nome do cliente
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    //Recupera o horario selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+
+    //Recupera o horario selecionado
+    if (!hourSelected) {
+      return alert("Selecione a hora");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    //Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = new Date().getTime();
+
+    console.log(id, name, when);
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+    console.log(error);
+  }
 };
