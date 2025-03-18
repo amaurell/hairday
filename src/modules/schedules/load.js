46bdb98b320fe862e88f34752bc48f6ac132de19
Andre Maurell <amaurell@gmail.com>
2025-03-18 15:43:05 -0300

    Buscando agendamentos

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,17 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+
 import { hoursLoad } from "../form/hours-load.js";
 
 //Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtem a data do input
   const date = selectedDate.value;
+
+  // Buscar na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
 
   //Renderiza as horas disponíveis
   hoursLoad({ date });
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -1,6 +1,7 @@
 import { apiConfig } from "./api-config.js";
 export async function scheduleNew({ id, name, when }) {
   try {
+    // Faz requisição para enviar os dados do agendamento
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: "POST",
       headers: {
